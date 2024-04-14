eb8b337f73f53d36ff5b97562067c887f48fecb3
FelipeGasp <felipe.gasparotto@sptech.school>
2024-04-14 15:16:42 -0300

    Atualizando HTML(inserção do JS e div_resultado)

--- calculadora/script.js
+++ calculadora/script.js
@@ -4,7 +4,8 @@ function calculo() {
     // ESPAÇO PARA ADIÇÃO DA FORMULA E VARIAVEIS
     var valorInvestimento = Number(ipt_investimentoValor.value);
     var precoKilo = Number(ipt_precoKilo.value);
-
+    var videirasPorHectar = Number(ipt_videirasPorHectar.value);
+    var qtdKilosPorVideira = Number(ipt_qtdKilosPorVideira.value)
 
     // VALIDAÇÃO DAS INPUTS
     if (valorInvestimento == 0 ||  precoKilo == 0) {
@@ -12,7 +13,12 @@ function calculo() {
         console.warn(`Insira novamente! Valores invalidos`)
         //  else if(REGRA DE NEGOCIO PARA VALIDAR )
     } else{
-
+        div_resultado.style = "display:flex"
+        box_inputs.style = "display:none"
     }
     contagemCalculo++;
+}
+function retorno(){
+    div_resultado.style = "display:none"
+    box_inputs.style = "display:flex"
 }
