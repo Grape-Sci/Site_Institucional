d24da4b9b027b6c887a5d493a2c180592e5092c4
GustavoPresilli <gustavo.presilli@sptech.school>
2024-05-01 18:05:31 -0300

    Alteração no modo de alterar de Dashboards

--- Projeto/Site/public/JS/dash.js
+++ Projeto/Site/public/JS/dash.js
@@ -1,15 +1,23 @@
-function sair (){
+function sair() {
     window.location = "Inicio.html";
 }
-function monitorar (){
+
+function monitorar(idPlantacaoSelecionada) {
+    sessionStorage.PLANTACAO_ATUAL = idPlantacaoSelecionada;
     window.location = "dashPlantacao.html";
 }
-function analisar (){
-    window.location = "dashTalhao1.html";
+
+function exibirPlantacao() {
+    var idPlantacao = sessionStorage.PLANTACAO_ATUAL;
+    tituloPlantacao.innerHTML = `Plantação ${idPlantacao}`
 }
-function analisar2 (){
-    window.location = "dashTalhao2.html";
+
+function analisar(idTalhaoSelecionado) {
+    sessionStorage.TALHAO_ATUAL = idTalhaoSelecionado;
+    window.location = "dashTalhao.html";
 }
-function analisar3 (){
-    window.location = "dashTalhao3.html";
+
+function exibirTalhao() {
+    idTalhao = sessionStorage.TALHAO_ATUAL
+    tituloTalhao.innerHTML = `Talhão ${idTalhao}`
 }
