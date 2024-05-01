66242e545aba1afedb5080ac096117e0706e6bc1
GustavoPresilli <gustavo.presilli@sptech.school>
2024-05-01 19:13:45 -0300

    Comentando código da dashboard de Talhão

--- Projeto/Site/public/JS/dash.js
+++ Projeto/Site/public/JS/dash.js
@@ -3,21 +3,29 @@ function sair() {
 }
 
 function monitorar(idPlantacaoSelecionada) {
-    sessionStorage.PLANTACAO_ATUAL = idPlantacaoSelecionada;
+    // sessionStorage.PLANTACAO_ATUAL = idPlantacaoSelecionada;
     window.location = "dashPlantacao.html";
 }
 
-function exibirPlantacao() {
-    var idPlantacao = sessionStorage.PLANTACAO_ATUAL;
-    tituloPlantacao.innerHTML = `Plantação ${idPlantacao}`
-}
+// function exibirPlantacao() {
+//     var idPlantacao = sessionStorage.PLANTACAO_ATUAL;
+//     tituloPlantacao.innerHTML = `Plantação ${idPlantacao}`
+// }
 
 function analisar(idTalhaoSelecionado) {
-    sessionStorage.TALHAO_ATUAL = idTalhaoSelecionado;
-    window.location = "dashTalhao.html";
+    // sessionStorage.TALHAO_ATUAL = idTalhaoSelecionado;
+    window.location = "dashTalhao1.html";
 }
 
-function exibirTalhao() {
-    idTalhao = sessionStorage.TALHAO_ATUAL
-    tituloTalhao.innerHTML = `Talhão ${idTalhao}`
+function analisar2() {
+    window.location = "dashTalhao2.html";
 }
+
+function analisar3() {
+    window.location = "dashTalhao3.html";
+}
+
+// function exibirTalhao() {
+//     idTalhao = sessionStorage.TALHAO_ATUAL
+//     tituloTalhao.innerHTML = `Talhão ${idTalhao}`
+// }
